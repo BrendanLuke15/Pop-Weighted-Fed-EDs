5957bac66eb50a8348ac6d6b7815b18fb96e513e
Brendan Luke <31905278+BrendanLuke15@users.noreply.github.com>
2021-09-29 15:00:09 -0400

    Add files via upload

--- PythonScript.py
+++ PythonScript.py
@@ -9,7 +9,9 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 #import xml as xml
-import xml.etree.ElementTree as ET
+#import xml.etree.ElementTree as ET
+import re
+import codecs
 
 # Get CSV file and parse out districts and population
 EDpopData = np.genfromtxt("ED-Canada_2016.csv", delimiter=",", dtype=None)
@@ -19,7 +21,16 @@ ED_NameFr = EDpopData[2:len(EDpopData[:,1]),2]
 ED_Pop = EDpopData[2:len(EDpopData[:,1]),3]
 
 # Get KML file and parse out lat-lon coordinates of district outlines
-KMLData = ET.parse('FED_CA_2019_EN.xml')
+#KMLData = open('FED_CA_2019_EN.kml','rt',encoding=)
+KMLData = codecs.open('FED_CA_2019_EN.kml','r',encoding='utf-8')
+KMLData = KMLData.read()
+test = KMLData.split("<Placemark")
+
+for x in test:
+    name = re.search('<name>(.+?)</name>',x)
+    print(name.group(1))
+#print(len(test))
+
 
 
 
