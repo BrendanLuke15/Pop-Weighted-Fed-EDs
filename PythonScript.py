5027238b0bb8a874a742f17b75c0d5dab07c53c0
Brendan Luke <31905278+BrendanLuke15@users.noreply.github.com>
2021-10-10 11:43:53 -0400

    Add files via upload

--- PythonScript.py
+++ PythonScript.py
@@ -1,5 +1,5 @@
 # By: Brendan Luke
-# Date: October 1, 2021
+# Date: October 10, 2021
 # Purpose: script to create population weighted electoral maps of Canada's federal electoral districts
 from datetime import datetime
 startTime = datetime.now()
@@ -10,16 +10,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 import re
 import codecs
+import os
+
+# relative filepaths sometimtes don't work
+dirname = os.path.dirname(__file__)
 
 # Get CSV file and parse out districts and population
-EDpopData = np.genfromtxt("data/ED-Canada_2016.csv", delimiter=",", dtype=None)
+EDpopData = np.genfromtxt(dirname + "/data/ED-Canada_2016.csv", delimiter=",", dtype=None)
 ED_ID = EDpopData[2:len(EDpopData[:,1]),0]
 ED_NameEn = EDpopData[2:len(EDpopData[:,1]),1]
 ED_NameFr = EDpopData[2:len(EDpopData[:,1]),2]
 ED_Pop = EDpopData[2:len(EDpopData[:,1]),3]
 
 # Save KML file as .txt file and cut out unwanted junk
-KMLData = codecs.open('data/FED_CA_2019_EN.kml','r',encoding='utf-8')
+KMLData = codecs.open(dirname + '/data/FED_CA_2019_EN.kml','r',encoding='utf-8')
 KMLData = KMLData.read()
 Regexs = ["<description>(.+?)</description>\\n","<styleUrl>(.+?)</styleUrl>\\n",
     "<Snippet(.+?)</Snippet>\\n","<Style(.+?)</Style>\\n","<\?xml(.+?)?>\\n","<kml(.+?)>\\n",
@@ -57,7 +61,10 @@ for ED in splitData:
         j = 0
         lat = []
         lon = []
-        # write lat and lon to lists
+        x = []
+        y = []
+        z = []
+        # write lat and lon to lists, cartesian coordinates
         for coord in tempCoords:
             if j%2 == 0: # even index (longitude)
                 lon.append(float(coord))
@@ -65,14 +72,27 @@ for ED in splitData:
             else: # odd index (latitude)
                 lat.append(float(coord))
                 j += 1
+                x.append(math.cos(math.radians(lat[int(j/2-1)]))*math.cos(math.radians(lon[int(j/2-1)])))
+                y.append(math.cos(math.radians(lat[int(j/2-1)]))*math.sin(math.radians(lon[int(j/2-1)])))
+                z.append(math.sin(math.radians(lat[int(j/2-1)])))
         # get center coordinates
         cenLon.append(sum(lon)/len(lon))
         cenLat.append(sum(lat)/len(lat))
         # create plot and save image
-        plt.plot(lon,lat,'k-',linewidth=2)
-        plt.axis('equal')
+        fig = plt.figure()
+        ax = fig.add_subplot(projection='3d')
+        #ax.set_box_aspect((np.ptp(x), np.ptp(y), np.ptp(z)))  # aspect ratio is 1:1:1 in data space
+        ax.plot3D(x,y,z)
+        ax.set_xlim3d(-1,1)
+        ax.set_ylim3d(-1,1)
+        ax.set_zlim3d(0.65,1)
+        ax.view_init(elev=62.4, azim=-96.466667) # set viww to middle of canada
+
+
+        #plt.plot(lon,lat,'k-',linewidth=2)
+        #plt.axis('equal')
         plt.axis('off')
-        plt.savefig("images/"+name[len(name)-1]+".png",transparent=True,bbox_inches='tight',pad_inches=0)
+        plt.savefig(dirname + "/images/"+name[len(name)-1]+".png",transparent=True,bbox_inches='tight',pad_inches=0)
         plt.close()
         i += 1
         print(i)
